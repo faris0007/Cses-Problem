22d0f82b9f37115c4780c1eca8a1f2041532c667
faris <113785816+faris0007@users.noreply.github.com>
2023-10-11 00:10:36 +0300

    Add problem Coin Combinations I

---
+++ CSES/Dynamic_Programming/Coin_Combinations_I.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)				((v).begin()), ((v).end())
+#define rall(v)				((v).rbegin()), ((v).rend())
+#define F first
+#define S second
+#define oo 1e18+5
+#define MOD ll(1e9+7)
+//#define endl '\n'
+#define fvec(i,vec) for(auto i:vec)
+#define pb push_back
+#define mpr make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+# define M_PI  3.14159265358979323846
+#define foor(i,a,b) for (ll i = a; i < b; i++)
+#define num_ocur_insort(vec,x) equal_range(all(vec), x) /* return pair upper lower   auto r=num_ocur_insort(a,6);*/
+typedef int ll;
+typedef unsigned long long ull;
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vip;
+typedef vector<string> vss;
+typedef map<ll,ll> mapi;
+typedef unordered_map<ll,ll> umapi;
+typedef vector<vip> viip;
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+const int N=5*1e5;
+
+ll add(ll a, ll b) {
+    return ((a % MOD + b % MOD) + MOD) % MOD;
+}
+
+void solve() {
+    ll n, x;
+    cin >> n >> x;
+    vi dp(x + 1, 0);
+    vi coin(n);
+    dp[0] = 1;
+    for (int i = 0; i < n; ++i) {
+        cin >> coin[i];
+    }
+    for (int i = 1; i <= x; ++i) {
+        for (auto c: coin) {
+            if (i - c >= 0)dp[i] = (dp[i]+dp[i-c])%MOD;;
+        }
+    }
+    if (dp[x] == oo) {
+        dp[x] = -1;
+    }
+    cout << dp[x] << endl;
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+//    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
