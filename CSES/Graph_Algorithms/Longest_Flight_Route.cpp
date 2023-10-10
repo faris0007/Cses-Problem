b9d4e80453978841c290ca37e4b62a668505ea88
faris <113785816+faris0007@users.noreply.github.com>
2023-10-11 00:11:21 +0300

    Add problem Longest Flight Route

---
+++ CSES/Graph_Algorithms/Longest_Flight_Route.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define all(v)				((v).begin()), ((v).end())
+#define rall(v)				((v).rbegin()), ((v).rend())
+#define F first
+#define S second
+#define oo 1e18+5
+#define MOD ll(1e9+7)
+#define endl '\n'
+#define fvec(i,vec) for(auto i:vec)
+#define pb push_back
+#define mpr make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+# define M_PI  3.14159265358979323846
+#define int long long
+#define foor(i,a,b) for (ll i = a; i < b; i++)
+#define num_ocur_insort(vec,x) equal_range(all(vec), x) /* return pair upper lower   auto r=num_ocur_insort(a,6);*/
+typedef int ll;
+typedef unsigned long long ull;
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vip;
+typedef vector<string> vss;
+typedef map<ll,ll> mapi;
+typedef unordered_map<ll,ll> umapi;
+typedef vector<vip> viip;
+typedef tree< pi, null_type, less<pi>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+const int N = 5 * 1e5;
+ll n, m;
+vi res;
+void dfs( int node,ll par,vii &adj,vi &vis,vi &dp) {
+    if (node == n){
+        dp[node] = 1;
+    }
+    if (vis[node] || node==n)return;
+    vis[node]=1;
+    for (auto ch: adj[node]) {
+        if (par == ch )continue;
+        dfs(ch, node, adj, vis, dp);
+        if (dp[ch])
+            dp[node] = max(dp[ch] + 1, dp[node]);
+    }
+}
+void build(ll node,ll par,vii &adj,vi &dp) {
+    res.push_back(node);
+    if(node==n)return;
+    for (auto ch: adj[node]) {
+        if (par == ch)continue;
+        if (dp[ch] + 1 == dp[node]) {
+            build(ch, node, adj, dp);
+            break;
+        }
+    }
+}
+
+void solve() {
+    cin >> n >> m;
+    vii adj(n + 1);
+    vi vis(n + 1, 0);
+    vi dp(n + 1, 0);
+    for (int i = 0; i < m; ++i) {
+        ll u, v, w;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    dfs(1, -1, adj, vis, dp);
+    build(1, -1, adj, dp);
+    if(dp[1]==0){
+        cout<<"IMPOSSIBLE"<<endl;
+        return;
+    }
+    cout<<res.size()<<endl;
+    for (int i = 0; i < res.size(); ++i) {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+}
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
