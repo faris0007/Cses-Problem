800563111ef903ba279bcc5854ee4088994761b7
faris <113785816+faris0007@users.noreply.github.com>
2023-10-11 00:12:06 +0300

    Add problem Dynamic Range Minimum Queries

---
+++ CSES/Range_Queries/Dynamic_Range_Minimum_Queries.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)				((v).begin()), ((v).end())
+#define rall(v)				((v).rbegin()), ((v).rend())
+#define F first
+#define S second
+#define oo 1e18+5
+#define MOD ll(10e18+7)
+//#define endl '\n'
+#define fvec(i,vec) for(auto i:vec)
+#define pb push_back
+#define mpr make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+# define M_PI  3.14159265358979323846
+#define foor(i,a,b) for (ll i = a; i < b; i++)
+#define num_ocur_insort(vec,x) equal_range(all(vec), x) /* return pair upper lower   auto r=num_ocur_insort(a,6);*/
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vip;
+typedef vector<string> vss;
+typedef map<ll,ll> mapi;
+typedef unordered_map<ll,ll> umapi;
+struct bebo {ll fir;ll sec;ll thi;ll fot;};
+typedef vector<vip> viip;
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+const int N=5*1e5;
+ll seg[N<<2];
+ll arr[N];
+void Build_seg(ll node,ll l , ll r) {
+    if (l == r) {
+        seg[node] = arr[l];
+        return;
+    }
+    ll mid = (l + r) / 2;
+    Build_seg(node * 2, l, mid);
+    Build_seg(node * 2 + 1, mid + 1, r);
+    seg[node] = min(seg[node * 2] , seg[node * 2 + 1]);
+}
+void update(ll node,ll l ,ll r,ll idx,ll val) {
+    if (l == r) {
+        seg[node] = val;
+        return;
+    }
+    ll mid = (l + r) / 2;
+    if (idx <= mid) update(node * 2, l, mid, idx, val);
+    else update(node * 2 + 1, mid + 1, r, idx, val);
+    seg[node] = min(seg[node * 2] , seg[node * 2 + 1]);
+}
+ll query(ll node,ll l ,ll r,ll st, ll en) {
+    ll mid = (l + r) / 2;
+    if (st > r || en < l)return oo;
+    if (l >= st && r <= en)return seg[node];
+    return min(query(node * 2, l, mid, st, en), query(node * 2 + 1, mid + 1, r, st, en));
+}
+void solve() {
+
+    ll n, q;
+    cin >> n >> q;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    Build_seg(1, 0, n - 1);
+    for (int i = 0; i < q; ++i) {
+        ll op, u, v;
+        cin >> op >> u >> v;
+        if (op == 1) {
+            update(1,0,n-1,u-1,v);
+        } else
+            cout << query(1, 0, n - 1, u - 1, v - 1) << endl;
+    }
+
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+}
