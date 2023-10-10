07dc9903a359b9a15f01e4ce916dd71bf94d1dbb
faris <113785816+faris0007@users.noreply.github.com>
2023-10-11 00:13:41 +0300

    Add problem Repeating Substring

---
+++ CSES/String_Algorithms/Repeating_Substring.cpp
@@ -0,0 +1,148 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define all(v)				((v).begin()), ((v).end())
+#define rall(v)				((v).rbegin()), ((v).rend())
+#define oo 1e18+5
+#define MOD ll(1e9+7)
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+# define M_PI  3.14159265358979323846
+#define int long long
+typedef int ll;
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vip;
+typedef map<ll,ll> mapi;
+typedef vector<vip> viip;
+typedef tree< pi, null_type, less<pi>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+const int N=2e5+10;
+///// Agamista
+vector<ll> clas;
+void radix_sort(vector<pair<pair<int,int>,int>>&a){
+    ll n=a.size();
+    {
+        vector<int>cnt(n);
+        for(auto item:a){
+            cnt[item.first.second]++;
+        }
+        vector<pair<pair<int,int>,int>>a_new(n);
+        vector<int>pos(n);
+        pos[0]=0;
+        for (int i = 1; i < n; ++i) {
+            pos[i]=pos[i-1]+cnt[i-1];
+        }
+        for(auto x:a){
+            int i=x.first.second;
+            a_new[pos[i]]=x;
+            pos[i]++;
+        }
+        a=a_new;
+    }
+    {
+        vector<int>cnt(n);
+        for(auto item:a){
+            cnt[item.first.first]++;
+        }
+        vector<pair<pair<int,int>,int>>a_new(n);
+        vector<int>pos(n);
+        pos[0]=0;
+        for (int i = 1; i < n; ++i) {
+            pos[i]=pos[i-1]+cnt[i-1];
+        }
+        for(auto x:a){
+            int i=x.first.first;
+            a_new[pos[i]]=x;
+            pos[i]++;
+        }
+        a=a_new;
+    }
+}
+vector<ll> suffarray(string &x) {
+    x += "$";
+    ll n = x.size();
+    vector<pair<char, ll>> a(n);
+    vector<ll> st(n);
+    vector<ll> clas(n);
+    for (int i = 0; i < n; ++i) { a[i] = {x[i], i}; }
+    sort(all(a));
+    for (int i = 0; i < n; ++i) { st[i] = a[i].second; }
+    clas[st[0]] = 0;
+    for (int i = 1; i < n; ++i) {
+        clas[st[i]] = clas[st[i - 1]];
+        if (a[i].first != a[i-1].first)clas[st[i]]++;
+    }
+    ll k = 0;
+    while ((1 << k) < n) {
+        vector<pair<pi, ll>> b(n);
+        for (int i = 0; i < n; ++i) {
+            b[i] = {{clas[i], clas[(i + (1 << k)) % n]}, i};
+        }
+        radix_sort(b);
+        for (int i = 0; i < n; ++i) { st[i] = b[i].second; }
+        clas[st[0]] = 0;
+        for (int i = 1; i < n; ++i) {
+            clas[st[i]] = clas[st[i - 1]];
+            if (b[i].first != b[i - 1].first)clas[st[i]]++;
+        }
+        k++;
+    }
+    return st;
+}
+ll fun(ll inx,string &z,string &x) {
+    ll sz = z.size();
+    ll f = 0;
+    ll n = x.size();
+    for (int i = inx; sz > 0; ++i, sz--, f++) {
+        if (x[i % n] < z[f])return 0;
+        else if (x[i % n] > z[f])return 1;
+    }
+    return 2;
+}
+vi get_lcp(string &x,vi &st) {
+    ll k = 0, n = x.size();
+    vi lcp(n);
+    vector<ll> clas(n);
+    for(int i=0 ; i <n ;i++) clas[st[i]] = i;
+    for (int i = 0; i < n - 1; ++i) {
+        ll pi = clas[i]; //// due to in  clas we start class from 0 ;
+        ll lst = st[pi - 1];
+        while (x[i + k] == x[lst + k])k++;
+        lcp[pi] = k;
+        k = max(0ll, k - 1);
+    }
+    return lcp;
+}
+void solve() {
+    string x;
+    cin >> x;
+    ll n = x.size();
+    vi suf = suffarray(x);
+    vi lcp = get_lcp(x, suf);
+    ll maxo = 0, fir = 0;
+    for (int i = 1; i < suf.size(); ++i) {
+        if (maxo < lcp[i]) {
+            maxo = max(lcp[i], maxo);
+            fir = suf[i];
+        }
+    }
+    if (maxo==0){
+          cout<<-1<<endl;
+        return;
+    }
+    cout << x.substr(fir, maxo) << endl;
+}
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll tc = 1;
+    //cin >> tc;
+    while (tc--) {
+        solve();
+    }
+}
