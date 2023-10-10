00108a9031d54b40879dcfffa3694f9b93b1a8a5
faris <113785816+faris0007@users.noreply.github.com>
2023-10-11 00:09:41 +0300

    Add problem Movie Festival

---
+++ CSES/Sorting_and_Searching/Movie_Festival.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define all(v)				((v).begin()), ((v).end())
+#define rall(v)				((v).rbegin()), ((v).rend())
+#define F first
+#define S second
+#define oo 1e18+5
+#define MOD ll(1e9+7)
+#define endl '\n'
+#define fvec(i,vec) for(auto i:vec)
+#define pb push_back
+#define mpr make_pair
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+# define M_PI  3.14159265358979323846
+#define int long long
+#define foor(i,a,b) for (ll i = a; i < b; i++)
+#define num_ocur_insort(vec,x) equal_range(all(vec), x) /* return pair upper lower   auto r=num_ocur_insort(a,6);*/
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vi;
+typedef vector<vi> vii;
+typedef pair<ll,ll> pi;
+typedef vector<pi> vip;
+typedef vector<string> vss;
+typedef map<ll,ll> mapi;
+typedef unordered_map<ll,ll> umapi;
+typedef vector<vip> viip;
+typedef tree< pi, null_type, less<pi>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+const int N=70000;
+
+
+void solve() {
+    ll n, x=1,f=1;
+    cin >> n;
+    vip a(n);
+    ordered_set s;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i].second >> a[i].first;
+    }
+    for (int i = 0; i < x; ++i) {
+        s.insert({0,f++});
+    }
+    sort(all(a));
+    ll sum = 0;
+    for (int i = 0; i < n; ++i) {
+        ll l = a[i].second;
+        ll r = a[i].first;
+        ll be = s.order_of_key({l,1e18});
+        if (be == 0)continue;
+        sum++;
+        s.erase(s.begin());
+        s.insert({r,f++});
+    }
+    cout << sum << endl;
+}
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
